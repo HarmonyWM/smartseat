ceb4de3ad6a2164ca017703a87cf1bd845d6a030
Its-JustSbu <116superstm@gmail.com>
2026-04-20 14:21:37 +0200

    assignment page integrated.

---
+++ frontend/src/app/utils/Hard-coded.ts
@@ -0,0 +1,11 @@
+export const sessions: any[] = [
+    {id: "", name: "Morning", time_slot: "09:00 - 10:30", "capacity": 20},
+    {id: "", name: "Midday",  time_slot: "11:00 - 12:30", "capacity": 20},
+    {id: "", name: "Afternoon",time_slot: "13:00 - 14:30","capacity": 20}
+]
+
+export const divisions: string[] = [
+    "Division A",
+    "Division B",
+    "Division C",
+]
